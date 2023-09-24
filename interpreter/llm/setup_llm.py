a8bb7b296427d39605d6a3b7b06e511f9e98ee8d
Sourcery AI <>
2023-09-24 14:14:53 +0000

    'Refactored by Sourcery'

--- interpreter/llm/setup_llm.py
+++ interpreter/llm/setup_llm.py
@@ -13,9 +13,6 @@ def setup_llm(interpreter):
 
     if not interpreter.local and "gpt-" in interpreter.model:
         # Function calling LLM
-        coding_llm = setup_openai_coding_llm(interpreter)
-    else:
-        text_llm = setup_text_llm(interpreter)
-        coding_llm = convert_to_coding_llm(text_llm, debug_mode=interpreter.debug_mode)
-
-    return coding_llm
+        return setup_openai_coding_llm(interpreter)
+    text_llm = setup_text_llm(interpreter)
+    return convert_to_coding_llm(text_llm, debug_mode=interpreter.debug_mode)
